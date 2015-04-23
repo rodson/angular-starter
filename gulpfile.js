a8c854ae7e6e8403543959df4eec9cdc407b8154
rodson <rodsonluo@gmail.com>
2015-04-23 17:03:52 +0800

    add automate task

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var AUTOPREFIXER_BROWSERS = [
 
 // Lint JavaScript
 gulp.task('jshint', function () {
-  return gulp.('app/**/*.js')
+  return gulp.src('app/**/*.js')
     .pipe(reload({stream: true, once: true}))
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
@@ -54,37 +54,81 @@ gulp.task('copy', function () {
 
 // Copy web fonts to dist
 gulp.task('fonts', function () {
-
+  return gulp.src(['app/assets/fonts/**'])
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe($.size({title: 'fonts'}));
 });
 
 // Compile and automatically prefix stylesheets
 gulp.task('styles', function () {
-
+  return gulp.src([
+    'app/assets/styles/*.scss',
+    'app/assets/styles/**/*.css'
+  ])
+    .pipe($.sourcemaps.init())
+    .pipe($.changed('.tmp/styles', {extension: '.css'}))
+    .pipe($.sass({
+      precision: 10,
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    // Concatenate and minify styles
+    .pipe($.if('*.css', $.csso()))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe($.size({title: 'styles'}));
 });
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', function () {
+  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
+  return gulp.src('app/**/*.html')
+    .pipe(assets)
+    // Concatenate and minify JavaScript
+    .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+    // Concatenate and minify styles
+    // In case you are still using useref build blocks
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    // Minify any HTML
+    .pipe($.if('*.html', $.minifyHtml()))
+    // Output files
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'html'}));
 });
 
 // Clean output directory
-gulp.task('clean', function () {
-
-});
+gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
 // Watch files for changes & reload
 gulp.task('serve', ['styles'], function () {
+  browserSync({
+    notify: false,
+    // Customize the BrowserSync console logging prefix
+    logPrefix: 'AS',
+    server: ['.tmp', 'app']
+  });
 
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/assets/styles/**/*.{scss,css}'], ['style', reload]);
+  gulp.watch(['app/**/*.js'], ['jshint']);
+  gulp.watch(['app/assets/images/**/*'], [reload]);
 });
 
 // Build and serve the output from the dist build
 gulp.task('serve:dist', ['default'], function () {
-
+  browserSync({
+    notify: false,
+    server: 'dist'
+  });
 });
 
 // Build production files, the default task
 gulp.task('default', ['clean'], function (cb) {
-
+  runSequence('styles', ['jshint', 'html', 'images', 'fonts', 'copy'], cb);
 });
 
 // Run PageSpeed Insights
